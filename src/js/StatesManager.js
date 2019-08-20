367ad756c30c2805047bab2bd127a31dc95fba52
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2019-08-20 13:04:53 +0200

    Revert "Fix build"
    This reverts commit d090b8617a0bfcaf68e3dd43943888a801f5e0b2.

--- src/js/StatesManager.js
+++ src/js/StatesManager.js
@@ -22,200 +22,195 @@ import { TangramCreatorState } from './States/TangramCreator.js'
 
 
 //Actions:
-import { BackgroundColorAction } from './States/Actions/BackgroundColor.js'
-import { BorderColorAction } from './States/Actions/BorderColor.js'
-import { BuildCenterAction } from './States/Actions/BuildCenter.js'
-import { CreateAction } from './States/Actions/Create.js'
-import { DeleteAction } from './States/Actions/Delete.js'
-import { CopyAction } from './States/Actions/Copy.js'
-import { GroupAction } from './States/Actions/Group.js'
-import { MoveAction } from './States/Actions/Move.js'
-import { ReverseAction } from './States/Actions/Reverse.js'
-import { RotateAction } from './States/Actions/Rotate.js'
-import { TranslatePlaneAction } from './States/Actions/TranslatePlane.js'
-import { UngroupAction } from './States/Actions/Ungroup.js'
-import { ZoomPlaneAction } from './States/Actions/ZoomPlane.js'
-import { OpacityAction } from './States/Actions/Opacity.js'
-import { ToBackgroundAction } from './States/Actions/ToBackground.js'
-import { DivideAction } from './States/Actions/Divide.js'
-import { MergeAction } from './States/Actions/Merge.js'
-import { CutAction } from './States/Actions/Cut.js'
+import  { BackgroundColorAction } from './States/Actions/BackgroundColor.js'
+import  { BorderColorAction } from './States/Actions/BorderColor.js'
+import  { BuildCenterAction } from './States/Actions/BuildCenter.js'
+import  { CreateAction } from './States/Actions/Create.js'
+import  { DeleteAction } from './States/Actions/Delete.js'
+import  { CopyAction } from './States/Actions/Copy.js'
+import  { GroupAction } from './States/Actions/Group.js'
+import  { MoveAction } from './States/Actions/Move.js'
+import  { ReverseAction } from './States/Actions/Reverse.js'
+import  { RotateAction } from './States/Actions/Rotate.js'
+import  { TranslatePlaneAction } from './States/Actions/TranslatePlane.js'
+import  { UngroupAction } from './States/Actions/Ungroup.js'
+import  { ZoomPlaneAction } from './States/Actions/ZoomPlane.js'
+import  { OpacityAction } from './States/Actions/Opacity.js'
+import  { ToBackgroundAction } from './States/Actions/ToBackground.js'
+import  { DivideAction } from './States/Actions/Divide.js'
+import  { MergeAction } from './States/Actions/Merge.js'
+import  { CutAction } from './States/Actions/Cut.js'
 
 export class StatesManager {
-    static get actions() {
-        return {
-            'BackgroundColorAction': {
-                "getInstance": function () { return new BackgroundColorAction(); }
-            },
-            'BorderColorAction': {
-                "getInstance": function () { return new BorderColorAction(); }
-            },
-            'BuildCenterAction': {
-                "getInstance": function () { return new BuildCenterAction(); }
-            },
-            'CreateAction': {
-                "getInstance": function () { return new CreateAction(); }
-            },
-            'DeleteAction': {
-                "getInstance": function () { return new DeleteAction(); }
-            },
-            'CopyAction': {
-                "getInstance": function () { return new CopyAction(); }
-            },
-            'GroupAction': {
-                "getInstance": function () { return new GroupAction(); }
-            },
-            'MoveAction': {
-                "getInstance": function () { return new MoveAction(); }
-            },
-            'ReverseAction': {
-                "getInstance": function () { return new ReverseAction(); }
-            },
-            'RotateAction': {
-                "getInstance": function () { return new RotateAction(); }
-            },
-            'TranslatePlaneAction': {
-                "getInstance": function () { return new TranslatePlaneAction(); }
-            },
-            'UngroupAction': {
-                "getInstance": function () { return new UngroupAction(); }
-            },
-            'ZoomPlaneAction': {
-                "getInstance": function () { return new ZoomPlaneAction(); }
-            },
-            'OpacityAction': {
-                "getInstance": function () { return new OpacityAction(); }
-            },
-            'ToBackgroundAction': {
-                "getInstance": function () { return new ToBackgroundAction(); }
-            },
-            'DivideAction': {
-                "getInstance": function () { return new DivideAction(); }
-            },
-            'MergeAction': {
-                "getInstance": function () { return new MergeAction(); }
-            },
-            'CutAction': {
-                "getInstance": function () { return new CutAction(); }
-            }
-        };
-    }
-    static get states() {
-        return {
-            'create_shape': {
-                "name": 'Ajouter une forme',
-                "instance": null,
-                "getInstance": function () { return new CreateState(); }
-            },
-            'move_shape': {
-                "name": 'Glisser',
-                "instance": null,
-                "getInstance": function () { return new MoveState(); }
-            },
-            'rotate_shape': {
-                "name": 'Tourner',
-                "instance": null,
-                "getInstance": function () { return new RotateState(); }
-            },
-            'zoom_plane': {
-                "name": 'Zoom',
-                "instance": null,
-                "getInstance": function () { return new ZoomPlaneState(); }
-            },
-            'delete_shape': {
-                "name": 'Supprimer',
-                "instance": null,
-                "getInstance": function () { return new DeleteState(); }
-            },
-            'background_color': {
-                "name": 'Couleur de fond',
-                "instance": null,
-                "getInstance": function () { return new BackgroundColorState(); }
-            },
-            'border_color': {
-                "name": 'Couleur des bords',
-                "instance": null,
-                "getInstance": function () { return new BorderColorState(); }
-            },
-            'group_shapes': {
-                "name": 'Lier des formes',
-                "instance": null,
-                "getInstance": function () { return new GroupState(); }
-            },
-            'ungroup_shapes': {
-                "name": 'Délier des formes',
-                "instance": null,
-                "getInstance": function () { return new UngroupState(); }
-            },
-            'reverse_shape': {
-                "name": 'Retourner',
-                "instance": null,
-                "getInstance": function () { return new ReverseState(); }
-            },
-            'build_shape_center': {
-                "name": 'Construire le centre',
-                "instance": null,
-                "getInstance": function () { return new BuildCenterState(); }
-            },
-            'copy_shape': {
-                "name": 'Dupliquer',
-                "instance": null,
-                "getInstance": function () { return new CopyState(); }
-            },
-            'opacity': {
-                "name": 'Opacité',
-                "instance": null,
-                "getInstance": function () { return new OpacityState(); }
-            },
-            'divide_segment': {
-                "name": 'Diviser',
-                "instance": null,
-                "getInstance": function () { return new DivideState(); }
-            },
-            'merge_shapes': {
-                "name": 'Fusionner',
-                "instance": null,
-                "getInstance": function () { return new MergeState(); }
-            },
-            'cut_shape': {
-                "name": 'Découper',
-                "instance": null,
-                "getInstance": function () { return new CutState(); }
-            },
-            'translate_plane': {
-                "name": 'Glisser le plan',
-                "instance": null,
-                "getInstance": function () { return new TranslatePlaneState(); }
-            },
-            'to_background': {
-                "name": 'Passer en arrière plan',
-                "instance": null,
-                "getInstance": function () { return new ToBackgroundState(); }
-            },
-            'tangram_creator': {
-                "name": 'Créer un tangram',
-                "instance": null,
-                "getInstance": function () { return new TangramCreatorState(); }
-            }
-        };
-    }
+    static actions = {
+        'BackgroundColorAction': {
+            "getInstance": function() { return new BackgroundColorAction(); }
+        },
+        'BorderColorAction': {
+            "getInstance": function() { return new BorderColorAction(); }
+        },
+        'BuildCenterAction': {
+            "getInstance": function() { return new BuildCenterAction(); }
+        },
+        'CreateAction': {
+            "getInstance": function() { return new CreateAction(); }
+        },
+        'DeleteAction': {
+            "getInstance": function() { return new DeleteAction(); }
+        },
+        'CopyAction': {
+            "getInstance": function() { return new CopyAction(); }
+        },
+        'GroupAction': {
+            "getInstance": function() { return new GroupAction(); }
+        },
+        'MoveAction': {
+            "getInstance": function() { return new MoveAction(); }
+        },
+        'ReverseAction': {
+            "getInstance": function() { return new ReverseAction(); }
+        },
+        'RotateAction': {
+            "getInstance": function() { return new RotateAction(); }
+        },
+        'TranslatePlaneAction': {
+            "getInstance": function() { return new TranslatePlaneAction(); }
+        },
+        'UngroupAction': {
+            "getInstance": function() { return new UngroupAction(); }
+        },
+        'ZoomPlaneAction': {
+            "getInstance": function() { return new ZoomPlaneAction(); }
+        },
+        'OpacityAction': {
+            "getInstance": function() { return new OpacityAction(); }
+        },
+        'ToBackgroundAction': {
+            "getInstance": function() { return new ToBackgroundAction(); }
+        },
+        'DivideAction': {
+            "getInstance": function() { return new DivideAction(); }
+        },
+        'MergeAction': {
+            "getInstance": function() { return new MergeAction(); }
+        },
+        'CutAction': {
+            "getInstance": function() { return new CutAction(); }
+        }
+    };
 
-    static get permanentStates() {
-        return {
-            'permanent_zoom_plane': {
-                "name": 'Zoom',
-                "instance": null,
-                "getInstance": function () { return new PermanentZoomPlaneState(); }
-            }
+    static states = {
+        'create_shape': {
+            "name": 'Ajouter une forme',
+            "instance": null,
+            "getInstance": function() { return new CreateState(); }
+        },
+        'move_shape': {
+            "name": 'Glisser',
+            "instance": null,
+            "getInstance": function() { return new MoveState(); }
+        },
+        'rotate_shape': {
+            "name": 'Tourner',
+            "instance": null,
+            "getInstance": function() { return new RotateState(); }
+        },
+        'zoom_plane': {
+            "name": 'Zoom',
+            "instance": null,
+            "getInstance": function() { return new ZoomPlaneState(); }
+        },
+        'delete_shape': {
+            "name": 'Supprimer',
+            "instance": null,
+            "getInstance": function() { return new DeleteState(); }
+        },
+        'background_color': {
+            "name": 'Couleur de fond',
+            "instance": null,
+            "getInstance": function() { return new BackgroundColorState(); }
+        },
+        'border_color': {
+            "name": 'Couleur des bords',
+            "instance": null,
+            "getInstance": function() { return new BorderColorState(); }
+        },
+        'group_shapes': {
+            "name": 'Lier des formes',
+            "instance": null,
+            "getInstance": function() { return new GroupState(); }
+        },
+        'ungroup_shapes': {
+            "name": 'Délier des formes',
+            "instance": null,
+            "getInstance": function() { return new UngroupState(); }
+        },
+        'reverse_shape': {
+            "name": 'Retourner',
+            "instance": null,
+            "getInstance": function() { return new ReverseState(); }
+        },
+        'build_shape_center': {
+            "name": 'Construire le centre',
+            "instance": null,
+            "getInstance": function() { return new BuildCenterState(); }
+        },
+        'copy_shape': {
+            "name": 'Dupliquer',
+            "instance": null,
+            "getInstance": function() { return new CopyState(); }
+        },
+        'opacity': {
+            "name": 'Opacité',
+            "instance": null,
+            "getInstance": function() { return new OpacityState(); }
+        },
+        'divide_segment': {
+            "name": 'Diviser',
+            "instance": null,
+            "getInstance": function() { return new DivideState(); }
+        },
+        'merge_shapes': {
+            "name": 'Fusionner',
+            "instance": null,
+            "getInstance": function() { return new MergeState(); }
+        },
+        'cut_shape': {
+            "name": 'Découper',
+            "instance": null,
+            "getInstance": function() { return new CutState(); }
+        },
+        'translate_plane': {
+            "name": 'Glisser le plan',
+            "instance": null,
+            "getInstance": function() { return new TranslatePlaneState(); }
+        },
+        'to_background': {
+            "name": 'Passer en arrière plan',
+            "instance": null,
+            "getInstance": function() { return new ToBackgroundState(); }
+        },
+        'tangram_creator': {
+            "name": 'Créer un tangram',
+            "instance": null,
+            "getInstance": function() { return new TangramCreatorState(); }
+        }
+    };
+
+    static permanentStates = {
+        'permanent_zoom_plane': {
+            "name": 'Zoom',
+            "instance": null,
+            "getInstance": function() { return new PermanentZoomPlaneState(); }
         }
     }
 
     static getStateInstance(stateName) {
-        if (!this.states[stateName]) {
+        if(!this.states[stateName]) {
             console.error("This state does not exists");
             return null;
         }
-        if (!this.states[stateName].instance) {
+        if(!this.states[stateName].instance) {
             let instance = this.states[stateName].getInstance();
             this.states[stateName].instance = instance;
         }
@@ -224,11 +219,11 @@ export class StatesManager {
     }
 
     static getPermanentStateInstance(stateName) {
-        if (!this.permanentStates[stateName]) {
+        if(!this.permanentStates[stateName]) {
             console.error("This permanent state does not exists");
             return null;
         }
-        if (!this.permanentStates[stateName].instance) {
+        if(!this.permanentStates[stateName].instance) {
             let instance = this.permanentStates[stateName].getInstance();
             this.permanentStates[stateName].instance = instance;
         }
@@ -236,7 +231,7 @@ export class StatesManager {
     }
 
     static getStateText(stateName) {
-        if (!this.states[stateName]) return null;
+        if(!this.states[stateName]) return null;
         return this.states[stateName].name;
     }
 }
