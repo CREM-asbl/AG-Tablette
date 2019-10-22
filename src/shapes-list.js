74db59478439e35cf5874f6a5b84bdf8c9d0bc24
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2019-10-22 11:33:54 +0200

    Ajout segments

--- src/shapes-list.js
+++ src/shapes-list.js
@@ -1,97 +1,100 @@
-import { LitElement, html } from 'lit-element'
-import { app } from './js/App'
+import { LitElement, html } from 'lit-element';
+import { app } from './js/App';
 
 class ShapesList extends LitElement {
+  static get properties() {
+    return {
+      show: Boolean,
+      family: String,
+    };
+  }
 
-    static get properties() {
-        return {
-            show: Boolean,
-            family: String
+  set state({ name, selectedFamily }) {
+    if (this.family !== selectedFamily) {
+      this.family = selectedFamily;
+      this.shape = null;
+    }
+    this.show = name === 'create_shape';
+  }
+
+  render() {
+    if (!this.show) {
+      return html``;
+    }
+
+    const shapes = app.workspace.environment.getFamily(this.family).getShapesNames();
+
+    return html`
+      <style>
+        :host {
+          display: flex;
+          justify-content: center;
         }
-    }
 
-    set state({ name, selectedFamily }) {
-        if (this.family !== selectedFamily) {
-            this.family = selectedFamily
-            this.shape = null
-        }
-        this.show = name === 'create_shape'
-    }
-
-    render() {
-        if (!this.show) {
-            return html``
+        .container {
+          background: white;
+          box-shadow: 0 1px 3px gray;
+          z-index: 100;
+          box-sizing: border-box;
+          overflow: auto;
         }
 
-        const shapes = app.workspace.environment.getFamily(this.family).getShapesNames();
+        h2 {
+          padding: 4px;
+          margin: 0;
+          text-align: center;
+          background: gray;
+          color: white;
+          font-size: 1.2rem;
+        }
 
-        return html`
-        <style>
-            :host {
-                display: flex;
-                justify-content: center;
-            }
+        ul {
+          display: flex;
+          margin: 0;
+          padding: 0;
+          list-style: none;
+          overflow-x: auto;
+          overflow-y: hidden;
+        }
 
-            .container {
-                background: white;
-                box-shadow: 0 1px 3px gray;
-                z-index: 100;
-                box-sizing: border-box;
-                overflow: auto;
-            }
+        li {
+          margin: 0;
+          padding: 0;
+          height: 54px;
+        }
+      </style>
 
-            h2 {
-                padding:4px;
-                margin: 0;
-                text-align: center;
-                background: gray;
-                color: white;
-                font-size: 1.2rem;
-            }
+      <div class="container">
+        <h2>${this.family}</h2>
+        <ul>
+          ${shapes.map(
+            shape => html`
+              <li>
+                <canvas-button
+                  title="${shape}"
+                  family="${this.family}"
+                  shape="${shape}"
+                  @click="${this._clickHandle}"
+                  ?active="${shape === this.shape}"
+                >
+                </canvas-button>
+              </li>
+            `,
+          )}
+        </ul>
+      </div>
+    `;
+  }
 
-            ul {
-                display: flex;
-                margin: 0;
-                padding: 0;
-                list-style: none;
-                overflow-x: auto;
-                overflow-y: hidden;
-            }
-
-            li {
-                margin: 0;
-                padding: 0;
-                height: 54px;
-            }
-        </style>
-
-        <div class="container">
-            <h2>Famille du ${this.family}</h2>
-            <ul>
-                ${shapes.map(shape => html`
-                    <li>
-                        <canvas-button title="${shape}"
-                                    family="${this.family}"
-                                    shape="${shape}"
-                                    @click="${this._clickHandle}"
-                                    ?active="${shape === this.shape}">
-                        </canvas-button>
-                    </li>
-                `)}
-            </ul>
-        </div>
-    `
-    }
-
-    /**
-     * Met à jour l'état de l'application lorsque l'on clique sur le nom d'une forme
-     */
-    _clickHandle(event) {
-        this.shape = event.target.shape
-        const familyRef = app.workspace.environment.getFamily(this.family);
-        const shapeRef = familyRef.getShape(event.target.shape);
-        app.state.setShape(shapeRef);
-        this.show = false;
-    }
+  /**
+   * Met à jour l'état de l'application lorsque l'on clique sur le nom d'une forme
+   */
+  _clickHandle(event) {
+    this.shape = event.target.shape;
+    const familyRef = app.workspace.environment.getFamily(this.family);
+    const shapeRef = familyRef.getShape(event.target.shape);
+    app.state.setShape(shapeRef);
+    this.show = false;
+  }
 }
-customElements.define('shapes-list', ShapesList)
+customElements.define('shapes-list', ShapesList);
