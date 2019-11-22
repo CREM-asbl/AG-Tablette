9ab6ee9ecfabc843ae086e54072c89c11015f6d0
Hubert <hubert@Shamry-3.local>
2019-11-22 15:28:57 +0100

    delete supprime mnt toutes les formes du groupe

--- src/js/States/Actions/Delete.js
+++ src/js/States/Actions/Delete.js
@@ -9,11 +9,7 @@ export class DeleteAction extends Action {
 
     this.name = 'DeleteAction';
 
-    //La forme qui doit être supprimée
-    this.shape = null;
-
-    //L'index de la forme dans workspace.shapes (utile pour undo)
-    this.shapeIndex = null;
+    this.involvedShapes = null;
 
     /*
         Si la forme à supprimer fait partie d'un userGroup:
@@ -34,7 +30,8 @@ export class DeleteAction extends Action {
 
   saveToObject() {
     let save = {
-      shape: this.shape.saveToObject(),
+      involvedShapes: this.involvedShapes.map(s => s.saveToObject()),
+      nbShapes: this.involvedShapes.length,
       userGroupId: this.userGroupId,
       userGroupLastShapeId: this.userGroupLastShapeId,
       userGroupIndex: this.userGroupIndex,
@@ -44,9 +41,11 @@ export class DeleteAction extends Action {
   }
 
   initFromObject(save) {
-    this.shape = new Shape({ x: 0, y: 0 }, []);
-    this.shape.initFromObject(save.shape);
-
+    this.involvedShapes = save.involvedShapes.map(shape => {
+      let newShape = new Shape({ x: 0, y: 0 }, []);
+      newShape.initFromObject(shape);
+      return newShape;
+    });
     this.userGroupId = save.userGroupId;
     this.userGroupLastShapeId = save.userGroupLastShapeId;
     this.userGroupIndex = save.userGroupIndex;
@@ -54,59 +53,46 @@ export class DeleteAction extends Action {
   }
 
   checkDoParameters() {
-    if (!this.shape) return false;
+    if (!this.involvedShapes) return false;
     return true;
   }
 
   checkUndoParameters() {
-    if (!this.shape || !Number.isFinite(this.shapeIndex)) return false;
+    if (!this.involvedShapes) return false;
     return true;
   }
 
   do() {
     if (!this.checkDoParameters()) return;
 
-    let shape = this.shape,
-      userGroup = app.workspace.getShapeGroup(shape);
+    let userGroup = app.workspace.getShapeGroup(this.involvedShapes[0]);
 
-    if (userGroup) userGroup.deleteShape(shape);
-    this.shapeIndex = app.workspace.getShapeIndex(shape);
-    app.workspace.deleteShape(shape);
+    this.involvedShapes.forEach(s => {
+      if (userGroup) userGroup.deleteShape(s);
+      app.workspace.deleteShape(s);
+    });
 
-    //Supprimer le userGroup s'il existe et s'il ne reste qu'une forme.
-    this.deleteUserGroup = false;
-    this.userGroupIndex = null;
     if (userGroup) {
       this.userGroupId = userGroup.id;
-      if (userGroup.shapes.length == 1) {
-        this.deleteUserGroup = true;
-        this.userGroupLastShapeId = userGroup.shapes[0].id;
-        this.userGroupIndex = app.workspace.getGroupIndex(userGroup);
-        app.workspace.deleteGroup(userGroup);
-      }
+      this.userGroupIndex = app.workspace.getGroupIndex(userGroup);
+      app.workspace.deleteGroup(userGroup);
     }
   }
 
   undo() {
     if (!this.checkUndoParameters()) return;
 
-    let shape = this.shape;
+    let userGroup;
 
-    app.workspace.addShape(shape, this.shapeIndex);
-
-    if (this.userGroupId) {
-      let userGroup;
-      if (this.deleteUserGroup) {
-        let s1 = app.workspace.getShapeById(this.userGroupLastShapeId),
-          s2 = shape;
-        userGroup = new ShapeGroup(s1, s2);
-        userGroup.id = this.userGroupId;
-        app.workspace.addGroup(userGroup, this.userGroupIndex);
-      } else {
-        userGroup = app.workspace.getGroup(this.userGroupId);
-      }
-
-      userGroup.addShape(shape);
+    if (this.involvedShapes.length >= 2) {
+      userGroup = new ShapeGroup(this.involvedShapes[0], this.involvedShapes[1]);
+      userGroup.id = this.userGroupId;
+      app.workspace.addGroup(userGroup, this.userGroupIndex);
     }
+
+    this.involvedShapes.forEach((s, id) => {
+      app.workspace.addShape(s);
+      if (userGroup && id >= 2) userGroup.addShape(s);
+    });
   }
 }
