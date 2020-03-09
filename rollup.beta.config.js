d975300dfacbd2917c6851393f6fb7e3bdc405d8
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2020-03-09 17:12:18 +0100

    Fix rollup.beta

--- rollup.beta.config.js
+++ rollup.beta.config.js
@@ -5,6 +5,5 @@ import { createDefaultConfig } from '@open-wc/building-rollup';
 // export default createCompatibilityConfig({ input: './index.html' });
 
 const config = createDefaultConfig({ input: './index.html' });
-config.outputDir = 'beta';
 config.output.dir = 'beta';
 export default config;
