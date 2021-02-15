176134d7d8b39c3fcd544da7c6e832d87444e58d
Hubert <hubert.vromman@gmail.com>
2021-02-15 11:19:47 +0100

    some coordinates tests

---
+++ test/Coordinates/add.test.js
@@ -0,0 +1,6 @@
+import { expect } from '@esm-bundle/chai';
+import { Coordinates } from '../../src/Core/Objects/Coordinates';
+
+it('Coordinates.add', () => {
+  expect(new Coordinates({x: 2, y: 4}).add(new Coordinates({x: 5, y: 1}))).to.deep.equal(new Coordinates({x: 7, y: 5}));
+});
---
+++ test/Coordinates/multiply.test.js
@@ -0,0 +1,8 @@
+import { expect } from '@esm-bundle/chai';
+import { Coordinates } from '../../src/Core/Objects/Coordinates';
+
+it('Coordinates.multiply', () => {
+  expect(new Coordinates({x: 2, y: 4}).multiply(1)).to.deep.equal(new Coordinates({x: 2, y: 4}));
+  expect(new Coordinates({x: 2, y: 4}).multiply(3)).to.deep.equal(new Coordinates({x: 6, y: 12}));
+  expect(new Coordinates({x: 2, y: 4}).multiply(-2)).to.deep.equal(new Coordinates({x: -4, y: -8}));
+});
