97186b1d32c494e45ac48f83deacb67ae4a4dcf7
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2023-11-06 14:19:53 +0100

    [Update] manifest

--- public/sw.js
+++
@@ -1,7 +0,0 @@
-const remove = async () => {
-  const registrations = await self.navigator.serviceWorker.getRegistrations()
-  registrations.forEach(element => {
-    element.unregister()
-  });
-}
-remove()
