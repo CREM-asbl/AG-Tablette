52b22950193bfc7adc0483398160b095efe35788
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2024-04-04 14:13:58 +0200

    [Fix] UI tool-choice-popup

--- src/components/popups/tool-choice-popup.js
+++ src/components/popups/tool-choice-popup.js
@@ -12,105 +12,102 @@ class ToolChoicePopup extends LitElement {
 
   constructor() {
     super();
-
-    this.updateProperties = () => {
-      this.iconSize = app.menuIconSize;
-      this.families = app.environment.families || [];
-      this.tools = [...app.tools.filter(tool => tool.name != 'create')];
-    };
-    this.updateProperties();
-
-    this.eventHandler = () => {
-      this.updateProperties();
-    };
-
-    window.addEventListener('tools-changed', this.eventHandler);
-    window.addEventListener('close-popup', () => this.close());
+    this.updateProperties()
   }
 
   static styles = [
     TemplatePopup.template_popup_styles(),
     css`
         #toolChoicePopupBody {
-          display: flex;
+          display: grid;
+          grid-template: auto auto 1fr / 1fr 1fr;
+          height: 100%;
+          grid-auto-flow: column;
+          gap: 8px;
+        }
+
+        h3 {
+          margin: 0;
+          width: 100%;
+          text-align: center;
         }
 
         .toolContainer {
           display: grid;
           grid-template-columns : repeat(4, 1fr);
           gap: 2px;
-          width: 208px;
+          width: 100%;
+          height: 100%;
           place-content: baseline;
           background-color: white;
           border-radius: 4px;
-          margin: 5px;
           box-shadow: 0px 0px 5px rgb(102, 102, 102);
+          overflow: auto;
+          scrollbar-width: thin;
         }
 
-        span {
+        button {
+          border: inherit;
+          background: inherit;
           cursor: pointer;
+          font-size: 16px;
         }
-      `,
+
+        #allHide {
+          justify-self : right;
+        }
+
+        #allShow {
+          justify-self: left;
+        }
+      `
   ]
 
   render() {
     return html`
-      <style>
-        .toolContainer {
-          height: ${(parseInt((this.tools.length + this.families.length) / 4) + ((this.tools.length + this.families.length) % 4 ? 1 : 0)) * (52)}px;
-        }
-      </style>
       <template-popup>
         <h2 slot="title">Choix des outils disponibles</h2>
         <div id="toolChoicePopupBody" slot="body">
-          <div>
-            <h3>Disponibles</h3>
-            <span id="allHide" @click="${this._actionHandle}">
-              Tout cacher &#8594;
-            </span>
-            <div class="toolContainer">
-              ${this.families.filter(family => family.isVisible).map((family) => html`
-                  <icon-button
-                    name="${family.shapeTemplates[0].name}"
-                    type="Create"
-                    title="${family.name}"
-                    @click="${this._actionHandle}"
-                  ></icon-button>`)}
-              ${this.tools.filter(tool => tool.isVisible && !tool.isDisable).map(tool => html`
-                  <icon-button
-                    name="${tool.name}"
-                    type="State"
-                    title="${tool.title}"
-                    cantInteract="true"
-                    @click="${this._actionHandle}"
-                  ></icon-button>
-                `)}
-            </div>
+          <h3>Disponibles</h3>
+          <button id="allHide" @click="${this._actionHandle}">Tout cacher &#8594;</button>
+          <div class="toolContainer">
+                ${this.families.filter(family => family.isVisible).map((family) => html`
+                    <icon-button
+                      name="${family.shapeTemplates[0].name}"
+                      type="Create"
+                      title="${family.name}"
+                      @click="${this._actionHandle}"
+                    ></icon-button>`)}
+                ${this.tools.filter(tool => tool.isVisible && !tool.isDisable).map(tool => html`
+                    <icon-button
+                      name="${tool.name}"
+                      type="State"
+                      title="${tool.title}"
+                      cantInteract="true"
+                      @click="${this._actionHandle}"
+                    ></icon-button>
+                  `)}
           </div>
-          <div>
-            <h3>Non disponibles</h3>
-            <span id="allShow" @click="${this._actionHandle}">
-              &#8592; Tout montrer
-            </span>
-            <div class="toolContainer">
-              ${this.families.filter(family => !family.isVisible).map((family) => html`
-                  <icon-button
-                    name="${family.shapeTemplates[0].name}"
-                    type="Create"
-                    title="${family.name}"
-                    @click="${this._actionHandle}"
-                  ></icon-button>`)}
-              ${this.tools.filter(tool => !tool.isVisible && !tool.isDisable).map((tool) => html`
-                  <icon-button
-                    name="${tool.name}"
-                    type="State"
-                    title="${tool.title}"
-                    cantInteract="true"
-                    ?active="${tool.name === app.tool?.name}"
-                    @click="${this._actionHandle}"
-                  ></icon-button>
-                `)}
-            </div>
+          <h3>Non disponibles</h3>
+          <button id="allShow" @click="${this._actionHandle}">&#8592; Tout montrer</button>
+          <div class="toolContainer">
+                ${this.families.filter(family => !family.isVisible).map((family) => html`
+                    <icon-button
+                      name="${family.shapeTemplates[0].name}"
+                      type="Create"
+                      title="${family.name}"
+                      @click="${this._actionHandle}"
+                    ></icon-button>`)}
+                ${this.tools.filter(tool => !tool.isVisible && !tool.isDisable).map((tool) => html`
+                    <icon-button
+                      name="${tool.name}"
+                      type="State"
+                      title="${tool.title}"
+                      cantInteract="true"
+                      ?active="${tool.name === app.tool?.name}"
+                      @click="${this._actionHandle}"
+                    ></icon-button>
+                  `)}
           </div>
         </div>
         <div slot="footer">
@@ -142,6 +139,17 @@ class ToolChoicePopup extends LitElement {
       setState({ environment: { ...app.environment }, tools: [...app.tools] });
     }
   }
+
+  firstUpdated() {
+    window.addEventListener('tools-changed', this.updateProperties.bind(this));
+    window.addEventListener('close-popup', () => this.close());
+  }
+
+  updateProperties() {
+    this.iconSize = app.menuIconSize;
+    this.families = app.environment.families || [];
+    this.tools = [...app.tools.filter(tool => tool.name != 'create')];
+  };
 
   close() {
     this.remove();
