d0d6a216d2a51bba5b6f35fec5a33d0e4be6b632
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2026-03-10 11:53:08 +0100

    Fixe UI select-shape

--- src/components/tool-ui-container.ts
+++ src/components/tool-ui-container.ts
@@ -15,7 +15,7 @@ export class ToolUiContainer extends SignalWatcher(LitElement) {
       width: 100%;
       height: 100%;
       pointer-events: none;
-      z-index: 100;
+      z-index: 1500;
     }
     
     /* Allow pointer events on children */
