844c61ce075f227ddb0fd3021fd3b7a60a4bf666
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2026-05-15 11:27:34 +0200

    chore: ajouter une condition pour ignorer les tests de modèle en CI

--- playwright.config.js
+++ playwright.config.js
@@ -6,6 +6,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  // Ignore template smoke test in CI because it depends on external network.
+  testIgnore: process.env.CI ? ['**/example.spec.js'] : [],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
